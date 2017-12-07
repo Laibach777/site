f4b21b9a65681c3146bc5cfffe9a484c5bedac30
Alex Kolesichenko <Lindmantill@mail.ru>
2017-12-07 14:12:38 +0300

    my save

---
+++ index.php
@@ -0,0 +1,441 @@
+<!DOCTYPE html>
+<html lang="ru" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+</head>
+<body>
+
+
+
+<section id="top">
+
+    <div class="container">
+        <div class="row">
+<div class="col-md-12 sizeMin">
+
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Rightness group</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Ссылка</a></li>
+                    <li><a href="#">Ссылка</a></li>
+                    <li><a href="#">Ссылка</a></li>
+                    <li><a href="#">Ссылка</a></li>
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+</div>
+
+
+
+
+
+            <div class="col-md-3 menu">
+
+                <h2>Rightness</h2>
+                <p class="groupLogo">group</p>
+
+
+                <ul class="menuList">
+                    <li><a>Главная</a></li>
+                    <li><a>Услуги</a></li>
+                    <li><a>Пресс-центр</a></li>
+                    <li><a>Контакты</a></li>
+
+                </ul>
+
+                <ul class="phone">
+                    <li>__________________</li>
+                    <li><span class="glyphicon glyphicon-earphone"></span>+3908887463</li>
+                    <li><span class="glyphicon glyphicon-earphone"></span>+2365764433</li>
+
+                </ul>
+
+
+            </div>
+
+
+            <div class="col-md-9 logo">
+
+                <h1>Регистрация бизнеса </br> за одну неделю</h1>
+<p>On no twenty spring of in esteem spirit likely estate.
+    Continue new you declared differed learning bringing honoured.
+    At mean mind so upon they rent am walk. Shortly am waiting inhabit smiling he chiefly of in.
+    Lain tore time gone him his dear sure. Fat decisively estimating affronting assistance not.
+    Resolve pursuit regular so calling me. West he plan girl been my then up no.
+</p>
+
+                <div class="section-btn">
+                    <a href="#" class="btn btn-primary tellMy">Перезвоните мне</a>
+
+                </div>
+
+</div>
+
+
+
+        </div>
+
+    </div>
+
+
+
+
+</section>
+<section id="content">
+
+
+    <div class="container">
+
+        <div class="row ">
+
+            <div class="col-md-12"> <p>__________</p>
+                <h3>Виды услуг</h3>
+            </div>
+<div class="col-md-12">
+
+
+    <div class="content">
+
+        <div class="info-row">
+
+            <div class="col-md-3 col-sm-6 ">
+
+                <div class="info">
+<div class="iconca">
+
+    <span class="glyphicon glyphicon-pencil"></span>
+</div>
+                    <div class="info-content">
+
+                        <p>Виды услуг</p>
+                        <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                            Otherwise concealed favourite frankness on be at dashwoods defective at.
+                            Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                        <a>Продолжить...</a>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+
+            <div class="col-md-3 col-sm-6  ">
+
+                <div class="info">
+
+                    <div class="iconca">
+
+                        <span class="glyphicon glyphicon-user"></span>
+                    </div>
+
+
+                    <div class="info-content">
+
+                        <p>Виды услуг</p>
+                        <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                            Otherwise concealed favourite frankness on be at dashwoods defective at.
+                            Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                            <a>Продолжить...</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div class="col-md-3 col-sm-6 ">
+
+                <div class="info">
+
+                    <div class="iconca">
+
+                        <span class="glyphicon glyphicon-file"></span>
+                    </div>
+                    <div class="info-content">
+
+                        <p>Виды услуг</p>
+                        <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                            Otherwise concealed favourite frankness on be at dashwoods defective at.
+                            Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                        <a>Продолжить...</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-3 col-sm-6 ">
+
+                <div class="info">
+
+                    <div class="iconca">
+
+                        <span class="glyphicon glyphicon-euro"></span>
+                    </div>
+                    <div class="info-content">
+
+                        <p>Виды услуг</p>
+                        <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                            Otherwise concealed favourite frankness on be at dashwoods defective at.
+                            Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                        <a>Продолжить...</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+</div>
+
+
+
+
+
+
+            <div class="col-md-12">
+
+
+                <div class="content">
+
+                    <div class="info-row">
+
+                        <div class="col-md-3 col-sm-6">
+
+                            <div class="info">
+                                <div class="iconca">
+
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                </div>
+                                <div class="info-content">
+
+                                    <p>Виды услуг</p>
+                                    <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                                        Otherwise concealed favourite frankness on be at dashwoods defective at.
+                                        Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                                    <a>Продолжить...</a>
+                                </div>
+
+                            </div>
+
+
+
+                        </div>
+
+
+                        <div class="col-md-3 col-sm-6  ">
+
+                            <div class="info">
+
+                                <div class="iconca">
+
+                                    <span class="glyphicon glyphicon-user"></span>
+                                </div>
+
+
+                                <div class="info-content">
+
+                                    <p>Виды услуг</p>
+                                    <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                                        Otherwise concealed favourite frankness on be at dashwoods defective at.
+                                        Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                                    <a>Продолжить...</a>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                        <div class="col-md-3 col-sm-6 ">
+
+                            <div class="info">
+
+                                <div class="iconca">
+
+                                    <span class="glyphicon glyphicon-file"></span>
+                                </div>
+                                <div class="info-content">
+
+                                    <p>Виды услуг</p>
+                                    <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                                        Otherwise concealed favourite frankness on be at dashwoods defective at.
+                                        Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                                    <a>Продолжить...</a>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="col-md-3 col-sm-6 ">
+
+                            <div class="info">
+
+                                <div class="iconca">
+
+                                    <span class="glyphicon glyphicon-euro"></span>
+                                </div>
+                                <div class="info-content">
+
+                                    <p>Виды услуг</p>
+                                    <p>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown oh he common future.
+                                        Otherwise concealed favourite frankness on be at dashwoods defective at.
+                                        Sympathize interested simplicity at do projecting increasing terminated.</p>
+
+                                    <a>Продолжить...</a>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+</section>
+
+<section id="company">
+
+<div class="container">
+<div class="row">
+
+    <div class="col-md-12">
+
+        <p>________________</p>
+    </div>
+
+</div>
+
+
+    <div class="row">
+
+        <div class="col-md-7">
+
+<h3>О нашей компании</h3>
+<p class="pZag">Наша миссия</p>
+            <p class="textCompany">
+                Smile spoke total few great had never their too. Amongst moments do in arrived at my replied.
+                Fat weddings servants but man believed prospect. Companions understood is as especially pianoforte connection introduced.
+                Nay newspaper can sportsman are admitting gentleman belonging his. Is oppose no he summer lovers twenty in. Not his difficulty boisterous surrounded bed.
+                Seems folly if in given scale. Sex contented dependent conveying advantage can use. </p>
+<a class="podrobnee">Подробнее</a>
+
+            <div class="col-md-12 komanda">
+                <h4>Наша команда</h4>
+                <div class="col-md-4 info-sotrudnik"><img src="img/one.jpg" alt="..." class="img-circle photo" width="55" height="55">
+                    <p>Алексей петров</br>Директор</p>
+                </div>
+                <div class="col-md-4 info-sotrudnik"><img src="img/one.jpg" alt="..." class="img-circle photo" width="55" height="55">
+                    <p>Алексей петров</br>Директор</p></div>
+                <div class="col-md-4 info-sotrudnik"><img src="img/one.jpg" alt="..." class="img-circle photo" width="55" height="55">
+                    <p>Алексей петров</br>Директор</p></div>
+                <a class="podrobnee">Подробнее</a>
+            </div>
+
+        </div>
+        <div class="col-md-5 form">
+
+<h4>Остались вопросы?</h4>
+            <div class="form-icons">
+
+                <form class="form-inline" role="form">
+                    <div class="form-group">
+
+                        <input type="email" class="form-control input" id="exampleInputEmail2" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+
+                        <input type="password" class="form-control input" id="exampleInputPassword2" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+
+                        <input type="email" class="form-control input" id="exampleInputEmail2" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+
+                        <input type="password" class="form-control input" id="exampleInputPassword2" placeholder="Password">
+                    </div>
+
+                    <div class="form-group">
+
+                        <input type="password" class="form-control text" id="exampleInputPassword2" placeholder="Password">
+
+                    <div class="form-group">
+                    <button type="submit" class="btn btn-default">Получить консультацию</button>
+                    </div>
+                </form>
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+</div>
+
+
+</section>
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
